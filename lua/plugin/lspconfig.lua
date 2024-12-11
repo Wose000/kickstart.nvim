bb990200bf04bf2b3817bc8e0b00e6178719bf26
Massimo <jiro.due@gmail.com>
2024-12-11 07:39:58 +0100

    installed bash formatter

--- lua/plugin/lspconfig.lua
+++ lua/plugin/lspconfig.lua
@@ -202,6 +202,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'beautysh',
         'clangd',
         'clang-format',
         'markdownlint',
